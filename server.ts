48ecda57bdffe88a9f9c68d5321256d8259e722c
Tobias <tobias.helsing@futurice.com>
2020-06-22 21:02:43 +0300

    test

--- server.ts
+++ server.ts
@@ -6,9 +6,7 @@ import { connectToDb } from "./utils/db";
 require("dotenv").config();
 
 const app = express();
-const port = 8000;
-
-app.get("/", (req, res) => res.send("Welcome to Notez app."));
+const port = process.env.PORT || 8000;
 
 const server = app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
